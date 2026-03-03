646f984f9c2d392930f15d1d00c09e91bf110242
TiagoMadeira <tiagopmadeira@gmail.com>
2026-03-03 19:27:53 +0000

    feature: instrumented auth-service

--- app/config.py
+++ app/config.py
@@ -2,6 +2,10 @@ import os
 from pydantic_settings import BaseSettings
 
 class Settings(BaseSettings):
+    DEBUG: bool = False #default
+    ENABLE_MONOTORING: bool = True #default
+    OTEL_EXPORTER_OTLP_ENDPOINT: str
+    OTEL_SERVICE_NAME: str
     SECRET_KEY : str
     ALGORITHM : str = "HS256"
     ACCESS_TOKEN_EXPIRE_MINUTES : int = 30
